8e91011697d32f76c7efe305bd61657d2552e7a4
Dennis Lo <kawai924@gmail.com>
2019-09-12 23:12:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // File: app.js
 var express = require('express');
 var app = express();  // Init an Express object.
+
+//make port available to be a variable
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) { // Set page-gen fcn for URL root request.
     res.send('Hello World!'); // Send webpage containing "Hello World!".
 });
-app.listen(3000, function () { // Set callback action fcn on network port.
-    console.log('App.js listening on port 3000!');
+app.listen(port, function () { // Set callback action fcn on network port.
+    console.log('App.js listening on port '+port);
 });
