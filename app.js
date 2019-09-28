973e7828d4fa5d26aaa70ce265b55853e31eca3c
kawai924 <kawai924@gmail.com>
2019-09-28 12:53:33 -0700

    added getdir function onto home page and it refresh after 500 ms after submission

--- app.js
+++ app.js
@@ -13,11 +13,38 @@ var port = process.env.PORT || 3000;
 var index = require('./routes/index');
 app.use('/', index);
 
+
+
+//Extra features after this point
+//reading dir in import
+var fs = require('fs');
+function getFiles (dir, files_){
+    fs = fs || require('fs');
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
 //get function on the url
 app.get('/test',  function (req, res) { // Set page-gen fcn for URL root request.
-    res.send('Hello Liam Matt and Marco!'); // Send webpage containing "Hello World!".
+    //res.send('Hello Liam Matt and Marco!'); // Send webpage containing "Hello World!".
+    dirlist = getFiles('./import');
+    var list = '<p>';
+    for(let file of dirlist) {
+        list = list + file+'</br>';
+    }
+    list = list +'</p>';
+    res.send(list);
+    
 });
-
 
 app.listen(port, function () { // Set callback action fcn on network port.
     console.log('App.js listening on port '+port);
--- public/app/js/index.js
+++ public/app/js/index.js
@@ -1,4 +1,24 @@
 
+//function to call node js function from JS using url request
+
+const url = '/test';
+const getdir = async url => {
+    try {
+        const response = await fetch(url);
+        const text = await response.text().then(function(text){
+                        result = text;
+                    });;
+        //console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+    const dirlist = document.getElementById('dirlist');
+    dirlist.innerHTML = result;
+}
+
+// on page load
+getdir(url);
+
 function createRepo() {
     const textInput = document.getElementById('create-repo');
     setTimeout(function() {
@@ -6,7 +26,7 @@ function createRepo() {
     }, 3000);
     const repoCreated = document.getElementById('repo-created');
     repoCreated.innerHTML = '<h1>Repository Created</h1>';
-
+    setTimeout(function() {
+        getdir(url);
+    }, 500);
 }
-
-
