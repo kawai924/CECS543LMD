7dfc442199dab0ff3f578b3346e57891a3cdaf50
Marco Curci <marcocurci@Marcos-MacBook-Pro-2.local>
2019-09-27 20:19:30 -0700

    removed unneeded comments

--- public/app/js/index.js
+++ public/app/js/index.js
@@ -1,7 +1,3 @@
-// // Code to use CLI
-// const textInput = document.querySelector('#create-repo');
-// const submit = document.querySelector('#create-repo-button');
-
 
 function createRepo() {
     console.log("button works");
@@ -15,11 +11,4 @@ function createRepo() {
 
 }
 
-
-// function submitCode(e) { 
-//     console.log("Creating repo for:" , textInput.value);
-    
-// }
-
-// submit.addEventListener('click', submitCode);
 
