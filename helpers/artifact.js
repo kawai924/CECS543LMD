9d0f221d3cc9d81ffbd119a3d82ede1481fabe75
Marco Curci <marcocurci@Marcos-MacBook-Pro-2.local>
2019-09-27 20:09:28 -0700

    Cleaned up and added comments

--- helpers/artifact.js
+++ helpers/artifact.js
@@ -2,44 +2,27 @@ var fs = require('fs');
 
 module.exports = createArtifactId;
 
-// function createArtifactId(fileName) {
-//     return new Promise( (resolve, reject) => {
-//         fs.readFileSync(fileName, 'utf8', function(error, data) {
-//             if (!error) {
-//                 let ext = fileName.substring(fileName.indexOf('.'));
-//                 // let weights = [1,7,3,7,11]; 
-//                 let weights = [1,3,7,11,13];
-//                 const len = data.length;
-//                 let weight;
-//                 let sum = 0;
-//                 for (let i = 0; i < len; i++) {
-//                     weight = i % weights.length;
-//                     sum += (data.charCodeAt(i) * weights[weight]);
-//                 }
-//                 sum = sum % (Math.pow(2, 31) - 1);
-//                 let artifactName = `${sum}-L${len}${ext}`;
-//                 resolve(artifactName);
-//             } else {
-//                 reject("Error in creating artifact name");
-//             }
-//         });
-//     });
-// }
-
-
-
+/**
+ * Read the file and return the artifactID name
+ * @param fileName name of the file we will generate the artifactID for
+ */
 function createArtifactId(fileName) {
+    
+    // Read the file and grab the extension
     let data = fs.readFileSync(fileName, 'utf8'); 
     let ext = fileName.substring(fileName.indexOf('.'));
-    // let weights = [1,7,3,7,11]; 
     let weights = [1,3,7,11,13];
     const len = data.length;
     let weight;
     let sum = 0;
+
+    // Loop through the text. Creating the checksum using the ASCII numeric 
+    // character multiplied by the weights
     for (let i = 0; i < len; i++) {
         weight = i % weights.length;
         sum += (data.charCodeAt(i) * weights[weight]);
     }
+    // Cap so the sum doesn't grow too large
     sum = sum % (Math.pow(2, 31) - 1);
     let artifactName = `${sum}-L${len}${ext}`;
     return artifactName;
