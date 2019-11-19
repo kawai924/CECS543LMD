5dda8be6ac3d4d47436efd279368d4c76cb09eba
liamng <nguyen.dch@gmail.com>
2019-11-19 12:25:38 -0800

    Fix test.js

--- private/js/Test.js
+++ private/js/Test.js
@@ -5,9 +5,9 @@ const fs = require("fs-extra");
 const path = require("path");
 const ROOTPATH = path.join(__dirname, "..", "..");
 
-const username = "liam";
+const username = "Alice";
 const repoName = "ProjectX"; // Grab from project folder.
-const projectPath = path.join(ROOTPATH, "database", "liam", "ProjectX"); // This should be the path to the project
+const projectPath = path.join(ROOTPATH, "database", username, repoName); // This should be the path to the project
 
 fs.removeSync(path.join(projectPath));
 
