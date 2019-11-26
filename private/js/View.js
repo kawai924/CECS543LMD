575f42338deed766eeeb3872d22db94f3e66e1a5
liamng <nguyen.dch@gmail.com>
2019-11-25 23:41:50 -0800

    Add View Handlers

--- private/js/View.js
+++ private/js/View.js
@@ -5,10 +5,40 @@ const path = require("path");
 const fs = require("fs");
 
 class View {
+  constructor() {
+    this.viewState = new ViewAll();
+  }
+
+  addUsername(username) {
+    this.username = username;
+    this.viewState = new ViewOneUser(this.username);
+    return this;
+  }
+
+  addProject(projectName) {
+    this.projectName = projectName;
+    this.viewState = new ViewOneUserOneProj(this.username, this.projectName);
+    return this;
+  }
+
+  removeProject() {
+    this.projectName = null;
+    this.viewState = new ViewOneUser(this.username);
+    return this;
+  }
+
+  removeUsername() {
+    this.username = null;
+    this.viewState = new ViewAll();
+    return this;
+  }
+
   //if there is username, no project name, execute return an arary of manifests of all projects
   //if there is username, with project name, execute return an arary of manifests of a projects
   //if there is no username, execute return an array of an array of manifests of all projects
-  execute() {}
+  execute() {
+    this.viewState.execute();
+  }
 }
 
 class ViewOneUserOneProj {
@@ -20,7 +50,6 @@ class ViewOneUserOneProj {
   execute() {
     const masManReader = new MasterManReader(this.username, this.projectName);
     const manReader = new ManifestReader(this.username, this.projectName);
-
     const masMan = masManReader.getMasMan();
 
     return {
@@ -41,40 +70,34 @@ class ViewOneUser {
     const userPath = path.join(DB_PATH, this.username);
     const projects = fs.readdirSync(userPath);
 
-    const output = [
-      {
-        user: this.username
-      }
-    ];
+    let projsWithMans = [];
+    projects.map(proj => {
+      projsWithMans.push(new ViewOneUserOneProj(this.username, proj).execute());
+    });
 
     return {
       user: this.username,
-      projects: projects.map(proj =>
-        new ViewOneUserOneProj(this.username, proj).execute()
-      )
+      projects: projsWithMans
     };
   }
 }
 
 class ViewAll {
+  constructor() {}
+
   execute() {
     const dbPath = DB_PATH;
     const users = fs
       .readdirSync(dbPath)
       .filter(filename => /^(?!\.).*$/.test(filename)); //ignore DOT FILES
 
-    const usersAndProjs = users.map(user => {
-      const projects = fs.readdirSync(path.join(dbPath, user));
-      return { user, projects };
+    return users.map(user => {
+      const projects = new ViewOneUser(user).execute();
+      return {
+        user,
+        projects
+      };
     });
-
-    const output = [];
-    usersAndProjs.forEach(user => {
-      const projsList = user.projects;
-      projsList.map(proj => {});
-    });
-
-    console.log(JSON.stringify(usersAndProjs));
   }
 }
 
