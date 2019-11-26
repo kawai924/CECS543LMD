6c428ce659897b158fa632e3b6ed2712c65578e9
kawai924 <kawai924@gmail.com>
2019-11-26 15:00:21 -0800

    update on conflit json response

--- private/js/test_js/TestingConflict.js
+++ private/js/test_js/TestingConflict.js
@@ -1,22 +1,27 @@
 function numberOfConflict(mania, manib) {
 
   ssmallfilelist = [];
+  sfilelist = [];
   sfolderlist = [];
   for( let prop in mania.structure ){
       var structure = mania.structure[prop];
+      //console.log(mania.structure[prop]);
+      sfilelist.push(mania.structure[prop]);
       filename = structure.artifactNode;
       ssmallfilelist.push(filename);
       var tar = filename.lastIndexOf("/");
       var file = filename.substring(0, tar);
       sfolderlist.push(file);
   }
-  console.log(ssmallfilelist, sfolderlist);
+  //console.log(ssmallfilelist, sfolderlist);
   
   //manib = target manifest json object;
   tsmallfilelist = [];
+  tfilelist = [];
   tfolderlist = [];
   for( let prop in manib.structure ){
     var structure = manib.structure[prop];
+    tfilelist.push(manib.structure[prop]);
     //console.log(structure.artifactNode, structure.artifactAbsPath);
     filename = structure.artifactNode;
     tsmallfilelist.push(filename);
@@ -26,7 +31,7 @@ function numberOfConflict(mania, manib) {
     tfolderlist.push(file);
     
   }
-  console.log(tsmallfilelist, tfolderlist);
+  //console.log(tsmallfilelist, tfolderlist);
 
   var conflict = 0;
   
@@ -37,27 +42,27 @@ function numberOfConflict(mania, manib) {
     if (
       tsmallfilelist[tkey] == ssmallfilelist[key]
     ) {
+      //console.log(sfilelist[key], tfilelist[tkey]);
       data.push(
         {
-          source: ssmallfilelist[key],
-          target: tsmallfilelist[tkey]
+          source: sfilelist[key],
+          target: tfilelist[tkey]
         }
       );
       
       conflict++;
     }
   }
+  console.log(data);
   var json = 
     {
-      // source : sdir,
-      // target: tdir, 
-      keyconflict: conflict,
-      keyconflictfile: data
+      conflict: conflict,
+      conflictFiles: data
     };
   //console.log(data);
-  console.log(json);
+  //console.log(json);
 
-  // return json;
+  return json;
 }
 
 
@@ -77,4 +82,5 @@ const mani = {
   "datetime": "2019-11-07T23:08:56.408Z"
 };
 
-numberOfConflict(mani,mani);
+var response = numberOfConflict(mani,mani);
+console.log(response);
