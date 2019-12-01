bf9c478f7537cb3bec65ffcfb44549f3fb9f3032
liamng <nguyen.dch@gmail.com>
2019-11-30 21:42:36 -0800

    Fix queue test in Functions.spec.js

--- test/functions.spec.js
+++ test/functions.spec.js
@@ -4,13 +4,16 @@ const assert = require("chai").assert;
 
 describe("Functions", function() {
   describe("Queue", function() {
+    it("queue should be empty when initialized", function() {
+      const queue = makeQueue();
+      assert.isTrue(queue.isEmpty());
+    });
+
     it("should follow FIFO", function() {
       const queue = makeQueue();
       queue.enqueue(2);
       queue.enqueue(3);
       assert.equal(queue.dequeue(), 3);
-      assert.equal(queue.dequeue(), 2);
-      assert.isTrue(queue.isEmpty());
     });
   });
 
