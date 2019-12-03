c34504fe6d16091484f225473126b891ad7c2579
liamng <nguyen.dch@gmail.com>
2019-12-02 20:48:23 -0800

    Change name of New Test.js to New_Test.js

--- private/js/New_Test.js
+++ private/js/New_Test.js
@@ -1,4 +1,4 @@
-const { ROOTPATH, DATABASE_NAME, USERS_FILENAME, DB_PATH } = require("./");
+const { ROOTPATH, DATABASE_NAME, USERS_FILENAME, DB_PATH } = require(".");
 const fsExt = require("fs-extra");
 const path = require("path");
 const { ProjectHandler } = require("./ProjectHandler");
@@ -21,33 +21,36 @@ const bProPath = bPH.projectPath;
 
 reset();
 
+// Alice
 aPH.create(); // manifest_1
+alice4CheckIn(); // Alice checks in 4 times
 
-// Alice checks in 4 times
-alice4CheckIn();
-
-// Bob check out
+// Bob
 manifestID = aMasManReader.getHead();
 bPH.checkout(user1, aProj1, manifestID);
 bob3CheckIn();
 
-// Alice check in
-alice3CheckIn();
-// Alice make new project
-aliceProject2();
+// Alice
+alice3CheckIn(); // Alice check in
+aliceProject2(); // Alice make new project
 
 /****** Helper functions
  * *********************************/
 function alice4CheckIn() {
+  // Create ./data.txt
   fsExt.writeFileSync(path.join(aProPath, "data.txt"), "Hello World");
   aPH.checkin();
 
+  // Check in ./foo/test1.txt
   fsExt.mkdirpSync(path.join(aProPath, "foo"));
   fsExt.writeFileSync(path.join(aProPath, "foo", "test1.txt"), "I'm under foo");
   aPH.checkin();
+
+  // Label the manifest above as label_1
   manifestID = aMasManReader.getHead();
   aMasManWriter.addLabel(manifestID, "label_1");
 
+  // Check in ./bar/baz/test2.txt
   fsExt.mkdirpSync(path.join(aProPath, "bar", "baz"), {
     recursive: true
   });
@@ -57,29 +60,37 @@ function alice4CheckIn() {
   );
   aPH.checkin();
 
+  // Change and check in ./foo/test1.txt
   fsExt.mkdirpSync(path.join(aProPath, "foo"));
   fsExt.writeFileSync(
     path.join(aProPath, "foo", "test1.txt"),
     "I just changed..."
   );
   aPH.checkin();
+
+  // Label manifefst above as label_2
   manifestID = aMasManReader.getHead();
   aMasManWriter.addLabel(manifestID, "label_2");
 }
 
 function alice3CheckIn() {
+  // Check in ./alice_folder
   fsExt.mkdirpSync(path.join(aProPath, "alice_folder"));
   fsExt.writeFileSync(
     path.join(aProPath, "alice_folder", "alice_file1.txt"),
     "Alice is happy"
   );
   aPH.checkin();
+
+  // label the manifest above as label_3
   manifestID = aMasManReader.getHead();
   aMasManWriter.addLabel(manifestID, "label_3");
 
+  // Replace ./data.txt content and check in
   fsExt.writeFileSync(path.join(aProPath, "data.txt"), "Alice writes data.txt");
   aPH.checkin();
 
+  // Check in ./alice_secret/alice_journal.txt
   fsExt.mkdirpSync(path.join(aProPath, "alice_secret"), {
     recursive: true
   });
@@ -97,6 +108,7 @@ function aliceProject2() {
 }
 
 function bob3CheckIn() {
+  // Check in ./bob_folder/run.txt
   fsExt.mkdirpSync(path.join(bProPath, "bob_folder"));
   fsExt.writeFileSync(
     path.join(bProPath, "bob_folder", "run.txt"),
@@ -104,12 +116,14 @@ function bob3CheckIn() {
   );
   bPH.checkin();
 
+  // Check in ./bob_folder/walk.txt
   fsExt.writeFileSync(
     path.join(bProPath, "bob_folder", "walk.txt"),
     "Bob is tired, walking"
   );
   bPH.checkin();
 
+  // Replace ./data.txt with new content and check in.
   fsExt.writeFileSync(
     path.join(bProPath, "data.txt"),
     "Bob overwrites file..."
@@ -117,11 +131,11 @@ function bob3CheckIn() {
   bPH.checkin();
 }
 
+/**
+ * Reset alice and bob
+ */
 function reset() {
   fsExt.removeSync(path.join(DB_PATH, user1));
   fsExt.removeSync(path.join(DB_PATH, user2));
   fsExt.removeSync(path.join(ROOTPATH, DATABASE_NAME, USERS_FILENAME));
 }
-
-// const headManifestID = repoHandler.getHeadManifestID();
-// repoHandler.addLabel(headManifestID, "my_label");
